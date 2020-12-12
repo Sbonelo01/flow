6a691609010008fa39bc761552c9c7a01a3c3a46
Sbonelo01 <sbonelomkhize15@gmail.com>
2020-12-12 10:54:31 +0200

    initial commit

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        // Get all Restaurant
+        $restaurant = Restaurant::all();
+
+        //
+        return [
+            'success' => true,
+            'data'    => $restaurant
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        // Validation
+
+        // Store
+        $results = Restaurant::create([
+            'name'      => $request->name,
+            'address'   => $request->address,
+            'telephone' => $request->telephone
+        ]);
+
+        //
+        return [
+            'success' => true,
+            'data'    => $results
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+        // Find Restaurant
+        $restaurant = Restaurant::findOrFail($id);
+
+        //
+        return [
+            'success' => true,
+            'data'    => $restaurant
+        ];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        // Validation
+        
+        // Find
+        $restaurant = Restaurant::findOrFail($id);
+
+        // Update
+        $restaurant->name      = $request->name;
+        $restaurant->address   = $request->address;
+        $restaurant->telephone = $request->telephone;
+
+        $restaurant->save();
+
+        return [
+            'success' => true,
+            'data'    => $restaurant
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+        // Find model to delete
+        $restaurant = Restaurant::findOrFail($id);
+
+        // Delete model
+        $restaurant->delete();
+
+        return [
+            'success' => true,
+            'data'    => $restaurant
+        ];
+    }
+}
---
+++ app/Models/Restaurant.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'telephone'
+    ];
+
+}
